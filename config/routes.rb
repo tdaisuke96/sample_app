c6aa6f407fc7a0e1ed3e803957a548de2e63bfac
tdaisuke96 <daisuke.togami@prum.jp>
2022-10-02 12:06:40 +0000

    Add user microposts

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,8 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
-    resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 
 
 #下記は周りくどいため使用しない  
