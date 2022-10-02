f59df40358fc2dd8885f22e8e7d5af7c9a3c552c
tdaisuke96 <daisuke.togami@prum.jp>
2022-10-02 13:02:00 +0000

    add user following

--- config/routes.rb
+++ config/routes.rb
@@ -13,10 +13,16 @@ Rails.application.routes.draw do
   get '/login',     to: 'sessions#new'
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
+
 
 
 #下記は周りくどいため使用しない  
